197a8bd81eb3a0f71815f2b3e0dab5bb7dcf45e6
shinkeonkim <singun11@kookmin.ac.kr>
2022-06-09 22:32:54 +0900

    24603: Scaling Recipe

---
+++ 24000~24999/24600~24699/24603.py
@@ -0,0 +1,30 @@
+"""
+[24603: Scaling Recipe](https://www.acmicpc.net/problem/24603)
+
+Tier: Bronze
+Category: 구현
+"""
+
+
+def solution(x: int, y: int, gredients: list) -> list:
+    """gradidents를 받아 y/x배 scale up 하여 반환합니다.
+
+    Args:
+        x (int): x
+        y (int): y
+        gredients (list): 재료 개수
+
+    Returns:
+        list: y/x배 scale up된 배열
+    """
+
+    return [gredient * y // x for gredient in gredients]
+
+
+if __name__ == '__main__':
+    n, x, y = map(int, input().split())
+    gradients = [int(input()) for _ in range(n)]
+
+    answer = solution(x, y, gradients)
+
+    print('\n'.join(map(str, answer)))
